428cf6cf15710393526800f342fe6b583b5d638d
Cristina <crisanguti@gmail.com>
2022-01-22 21:58:12 +0100

    Initial commit

---
+++ src/Ejercicio3Sesion3JavaBasico.java
@@ -0,0 +1,6 @@
+public class Ejercicio3Sesion3JavaBasico {
+    public static void main(String[] args) {
+        String [] nombres = {"Pepe", "Juan", "Anthony"};
+        System.out.print(nombres[0]+" "+nombres[1]+" "+nombres[2]);
+    }
+}
